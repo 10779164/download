116e9cd2d294b1858d2d2decff97e1d522f927c2
root <10779164>
2018-07-10 17:27:04 +0800

    add ubuntu18.04

---
+++ ispconfig/ispconfig_basic_install/distros/ubuntu-18.04/askquestions_update.sh
@@ -0,0 +1,18 @@
+AskQuestions_update() {
+
+	CFG_SERVICE=$(whiptail --title "update ispconfig" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "Select you want update server" 10 50 3 "MAIL" "" ON "DNS" "" OFF "MAIL and DNS" "" OFF 3>&1 1>&2 2>&3)
+
+	while [ "x$CFG_MYSQL_ROOT_PWD" == "x" ]
+          do
+                CFG_MYSQL_ROOT_PWD=$(whiptail --title "MySQL root password" --backtitle "$WT_BACKTITLE" --inputbox "Please enter the root password for the database~" --nocancel 10 50 3>&1 1>&2 2>&3)
+          done
+
+	CFG_WEBSERVER=$(whiptail --title "update ispconfig" --backtitle "$WT_BACKTITLE" --nocancel --radiolist "Confirm webserver~" 10 50 2 "apache" "" ON "nginx" "" OFF  3>&1 1>&2 2>&3)
+
+	if [ "$CFG_SERVICE" == "MAIL" ] || [ "$CFG_SERVICE" == "MAIL and DNS" ];then
+                CFG_ROUNDCUBE_PWD=$(whiptail --title "ISPConfig roundcube database password" --backtitle "$WT_BACKTITLE" --inputbox "Please specify database dbroundcube's password" --nocancel 10 50 3>&1 1>&2 2>&3)
+        fi
+
+}
+
+
---
+++ ispconfig/ispconfig_basic_install/distros/ubuntu-18.04/install_mta.sh
@@ -0,0 +1,11 @@
+#---------------------------------------------------------------------
+# Function: InstallMTA
+#    Install MTA Dovecot
+#---------------------------------------------------------------------
+InstallMTA() {
+	  echo -n "Installing Dovecot... ";
+	  echo "dovecot-core dovecot-core/create-ssl-cert boolean false" | debconf-set-selections
+	  echo "dovecot-core dovecot-core/ssl-cert-name string $CFG_HOSTNAME_FQDN" | debconf-set-selections
+	  apt-get -yqq install dovecot-imapd dovecot-pop3d dovecot-sieve dovecot-mysql dovecot-lmtpd opendkim opendkim-tools > /dev/null 2>&1
+	  echo -e "[${green}DONE${NC}]\n"
+}
