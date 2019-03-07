1d94f221fcae7025ef96fa4962925fd5837fb920
tony <tony.g.dbm@gmail.com>
2019-03-07 16:53:35 +0800

    update phpmyadmin

--- ispconfig/ispconfig_basic_install/distros/ubuntu-18.04/install_webserver.sh
+++ ispconfig/ispconfig_basic_install/distros/ubuntu-18.04/install_webserver.sh
@@ -17,7 +17,7 @@ InstallWebServer() {
 	systemctl restart apache2
 	echo -e "[${green}DONE${NC}]\n"
 	
-  if [ "$CFG_PHPMYADMIN" == "yes" ]; then
+  #if [ "$CFG_PHPMYADMIN" == "yes" ]; then
 	echo "==========================================================================================="
 	echo "Attention: When asked 'Configure database for phpmyadmin with dbconfig-common?' select 'NO'"
 	echo "Due to a bug in dbconfig-common, this can't be automated."
@@ -27,26 +27,26 @@ InstallWebServer() {
 	echo -n "Installing phpMyAdmin... "
 	apt-get -y install phpmyadmin
 	echo -e "[${green}DONE${NC}]\n"
-  fi
 	
   
   else
 	
 	echo -n "Installing NGINX and Modules... "
-	service apache2 stop > /dev/null 2>&1
-	update-rc.d -f apache2 remove > /dev/null 2>&1
+	#service apache2 stop > /dev/null 2>&1
+	#update-rc.d -f apache2 remove > /dev/null 2>&1
 	apt-get -yqq install nginx > /dev/null 2>&1
 	service nginx start 
-	apt-get -yqq install php7.2 php7.2-common php7.2-gd php7.2-mysql php7.2-imap php7.2-cli php7.2-cgi php-pear php-auth php7.2-mcrypt mcrypt imagemagick libruby php7.2-curl php7.2-intl php7.2-pspell php7.2-recode php7.2-sqlite3 php7.2-tidy php7.2-xmlrpc php7.2-xsl memcached php-memcache php-imagick php-gettext php7.2-zip php7.2-mbstring php7.2-fpm php7.2-opcache php-apcu > /dev/null 2>&1
-	sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php7/fpm/php.ini
-	sed -i "s/;date.timezone =/date.timezone=\"Europe\/Rome\"/" /etc/php7/fpm/php.ini
-	service php7-fpm reload
+	#apt-get -yqq install php7.2 php7.2-common php7.2-gd php7.2-mysql php7.2-imap php7.2-cli php7.2-cgi php-pear php-auth php7.2-mcrypt mcrypt imagemagick libruby php7.2-curl php7.2-intl php7.2-pspell php7.2-recode php7.2-sqlite3 php7.2-tidy php7.2-xmlrpc php7.2-xsl memcached php-memcache php-imagick php-gettext php7.2-zip php7.2-mbstring php7.2-fpm php7.2-opcache php-apcu > /dev/null 2>&1
+        apt-get -y install php7.2 php7.2-common php7.2-gd php7.2-mysql php7.2-imap php7.2-cli php7.2-cgi php-pear mcrypt imagemagick libruby php7.2-curl php7.2-intl php7.2-pspell php7.2-recode php7.2-sqlite3 php7.2-tidy php7.2-xmlrpc php7.2-xsl memcached php-memcache php-imagick php-gettext php7.2-zip php7.2-mbstring php-soap php7.2-soap php7.2-fpm
+	sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.2/fpm/php.ini
+	sed -i "s/;date.timezone =/date.timezone=\"Europe\/Rome\"/" /etc/php/7.2/fpm/php.ini
+	#service php7-fpm reload
 	apt-get -yqq install fcgiwrap
-	echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
+	#echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
         # - DISABLED DUE TO A BUG IN DBCONFIG - echo "phpmyadmin phpmyadmin/dbconfig-install boolean false" | debconf-set-selections
-    	echo "dbconfig-common dbconfig-common/dbconfig-install boolean false" | debconf-set-selections
-		apt-get -y install phpmyadmin
-    	echo "With nginx phpmyadmin is accessibile at  http://$CFG_HOSTNAME_FQDN:8081/phpmyadmin or http://IP_ADDRESS:8081/phpmyadmin"
+    	#echo "dbconfig-common dbconfig-common/dbconfig-install boolean false" | debconf-set-selections
+	#	apt-get -y install phpmyadmin
+    	#echo "With nginx phpmyadmin is accessibile at  http://$CFG_HOSTNAME_FQDN:8081/phpmyadmin or http://IP_ADDRESS:8081/phpmyadmin"
   fi
   echo -e "[${green}DONE${NC}]\n"
 }
